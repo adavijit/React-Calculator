b588069e7f5db17297a93c8a50197a58b3165c8a
Avijit Das <adavijit@yahoo.com>
2019-07-28 14:59:21 +0530

    Updates

---
+++ precache-manifest.0e57fe21fc2ae84a61362051e6df9f20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e5ed8373866e1bd90d9132bfe1c6109",
+    "url": "/React-Calculator/index.html"
+  },
+  {
+    "revision": "b3124846776fa63df364",
+    "url": "/React-Calculator/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "b000fccb79e89af31dc4",
+    "url": "/React-Calculator/static/js/2.abf43ca4.chunk.js"
+  },
+  {
+    "revision": "b3124846776fa63df364",
+    "url": "/React-Calculator/static/js/main.8fda5adf.chunk.js"
+  },
+  {
+    "revision": "19a8db08324e9b1cabe9",
+    "url": "/React-Calculator/static/js/runtime~main.cb778861.js"
+  }
+]);
